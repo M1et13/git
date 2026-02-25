e13a7cb0685225964a6dcb3ae22acf3bf499b359
M1et13 <petrukhintima@mail.ru>
2026-02-25 21:07:18 +0300

    поменял код на игру с числами

--- empty/empty.py
+++ empty/empty.py
@@ -1,2 +1,30 @@
-print("выпустите из подвала!!!")
-print("Это не шутка!!")
+import random
+points = {
+    "comp": 0,
+    "player": 0}
+def is_odd(number: int):
+    if number % 2 == 0:
+        return False
+    else:
+        return True
+while True:
+    a = random.randint(10, 110)
+    if is_odd(a):
+        continue
+    b = random.randint(10, 120)
+    if is_odd(b):
+        continue
+    op = random.choice(("+", "-"))
+    if op == "+":
+        ans = a + b
+        user = input(f"{a} + {b} = ")
+    else:
+        ans = a - b
+        user = input(f"{a} - {b} = ")
+    if user == str(ans):
+        print("Верно!")
+        points["player"] += 1
+    else:
+        print("Ты ошибся. Попробуй ещё разочек.")
+        points["comp"] += 1
+    print(f'Счёт: {points["player"]}:{points["comp"]}')
